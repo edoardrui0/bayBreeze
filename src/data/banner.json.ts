31d156cb8c36ad4f1ecb9cfa815f63aad260c1eb
Eddie Ruiz <44981900+edoardrui0@users.noreply.github.com>
2025-10-03 16:04:23 -0400

    Initial commit

---
+++ src/data/banner.json.ts
@@ -0,0 +1,18 @@
+export const bannerText = [
+    {
+        text: "About Us",
+        href: "/about"
+    },
+    {
+        text: "Services",
+        href: "/services",
+    },
+    {
+        text: "Contact Us",
+        href: "/contact",
+    },
+    {
+        text: "Pricing",
+        href: "/pricing",
+    },
+]
---
+++ src/data/navData.json.ts
@@ -0,0 +1,18 @@
+export const navLinks = [
+    {
+        text: "Home",
+        href: "/",
+    },
+    {
+        text: "About Us",
+        href: "/about",
+    },
+    {
+        text: "Services",
+        href: "/services",
+    },
+    {
+        text: "Pricing",
+        href: "/pricing",
+    },
+]
