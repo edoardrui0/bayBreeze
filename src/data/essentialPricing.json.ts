aeaca2eb5b327998b1f004267056fcd546ab32aa
Edoardo Ruiz <44981900+edoardrui0@users.noreply.github.com>
2025-10-22 16:15:04 -0400

    updated pricing

---
+++ src/data/essentialPricing.json.ts
@@ -0,0 +1,23 @@
+export const essentialPricing = [
+    {
+        text: "5-page Website",
+    },
+    {
+        text: "Includes Hosting & Maintenance",
+    },
+    {
+        text: "Ongoing 24/7 Support",
+    },
+    {
+        text: "Custom Code Development",
+    },
+    {
+        text: "Unlimited Edits: +$25/m",
+    },
+    {
+        text: "Additional Pages after 5: +$50/m",
+    },
+    {
+        text: "Blog Integration - +$50/m",
+    },
+]
